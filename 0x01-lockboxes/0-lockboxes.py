26cb2c22a5c0fbc3ff5ae6cd3a61d8c2acf408ab
absaGH <absa3852@gmail.com>
2021-12-04 14:11:09 +0000

    Lockboxes implementation

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python implementation of the Lockboxes challenge"""
+
+
+def canUnlockAll(boxes):
+    """function that returns True if all boxes can be unlocked, false otherwie"""
+    size = len(boxes)
+    flags = [1]
+    for i in range(1, size, 1):
+        flags.append(0)
+    for j in range(len(boxes)):
+        if (flags[j] == 1):
+            for k in range(len(boxes[j])):
+                if(flags[boxes[j][k]] == 0):
+                    flags[boxes[j][k]] = 1
+                    if(j > boxes[j][k]):
+                        for m in range(len(boxes[boxes[j][k]])):
+                            if(flags[boxes[boxes[j][k]][m]] == 0):
+                                flags[boxes[boxes[j][k]][m]] = 1
+
+    if (0 in flags):
+        return False
+    return True
