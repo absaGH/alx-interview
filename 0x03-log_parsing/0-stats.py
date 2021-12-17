25187c847f40f2469031cda599ec88f2e8f22938
absaGH <absa3852@gmail.com>
2021-12-17 09:25:50 +0000

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 '''Python script to parse log and display stat'''
-import fileinput
 import sys
 import re
 from collections import Counter
@@ -21,18 +20,18 @@ if __name__ == "__main__":
             if int(item) in [200, 301, 400, 401, 403, 404, 405, 500]:
                 print(item + ': ' + str(frq[item]))
 
-        try:
-            for line in sys.stdin:
+    try:
+        for line in sys.stdin:
 
-                line = line.strip()
-                pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})( - )\[\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}.\d+\] ("\w+ \/\w+\/\d{1,3} \w+\/1.1") \d{3} \d+'
-                if re.search(pattern, line):
-                    line = re.split(r'\s', line)
-                    count = count + 1
-                    flsz = flsz + int(line[-1])
-                    lst.append(line[-2])
-                    frq = Counter(lst)
-                    if (count % 10 == 0):
-                        printstat()
-except KeyboardInterrupt:
-    printstat()
+            line = line.strip()
+            pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})( - )\[\d{4}-\d{1,2}-\d{1,2} \d{1,2}:\d{1,2}:\d{1,2}.\d+\] ("\w+ \/\w+\/\d{1,3} \w+\/1.1") \d{3} \d+'
+            if re.search(pattern, line):
+                line = re.split(r'\s', line)
+                count = count + 1
+                flsz = flsz + int(line[-1])
+                lst.append(line[-2])
+                frq = Counter(lst)
+                if (count % 10 == 0):
+                    printstat()
+    except KeyboardInterrupt:
+        printstat()
