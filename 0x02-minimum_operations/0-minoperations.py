94966b440b1fb059562806a10a33bc5aba5dcea0
absaGH <absa3852@gmail.com>
2021-12-19 12:16:03 +0000

    find minimumn number of operations

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""A module to solve minimum operations challenge"""
+
+
+def minOperations(n):
+    """Function implementation of minimum operations finder algorithm"""
+    copy = 0
+    paste = 0
+    ops = 0
+    counter = 1
+
+    if n <= 1:
+        return 0
+
+    for i in range(n):
+        if counter < n:
+            if n % counter == 0:
+                copied = counter
+                copy = copy + 1
+                paste = paste + 1
+                counter += copied
+            else:
+                paste = paste + 1
+                counter += copied
+        ops = copy + paste
+    return ops
