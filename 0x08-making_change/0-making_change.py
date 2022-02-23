0ffcd49c5880436f1b6a5f0cf7e6d9469f40633b
absaGH <absa3852@gmail.com>
2022-02-23 14:35:51 +0000

    coin change implementation

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''
+calculates the number of coins needed to meet a
+given amount of total.
+'''
+
+
+def makeChange(coins, total):
+    '''
+    A function that reuturn the fewest number
+    of coins.
+    '''
+    sum = 0
+    count = 0
+    for i in range(0, len(coins)):
+        val = max(coins)
+        while (sum < total):
+            sum = sum + val
+            if(sum <= total):
+                count += 1
+            else:
+                sum = sum - val
+                indx = coins.index(val)
+                coins[indx] = 0
+                break
+
+    if(sum == total):
+        return count
+    else:
+        return -1
